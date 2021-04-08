e1e763aa80e05f740936d111bd56e8d1b9243004
selina24228 <selina24228@gmail.com>
2021-04-08 14:40:31 +0800

    support Mac & Ubuntu

--- Lab2/setRedirection.sh
+++ Lab2/setRedirection.sh
@@ -1,4 +1,16 @@
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to 10.0.2.15:9090
-iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to 10.0.2.15:9090
-iptables -t nat -I PREROUTING 1 -p all -d 10.42.0.105 -j ACCEPT
-iptables -t nat -I PREROUTING 2 -p all -s 10.42.0.105 -j ACCEPT
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # redirection
+    echo -n "[INFO] Redirecting..."
+    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to 10.0.2.15:9090
+    iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to 10.0.2.15:9090
+    
+    # update firewall rules
+    echo -n "[INFO] Updating firewall rules..."
+    iptables -t nat -I PREROUTING 1 -p all -d 10.42.0.105 -j ACCEPT
+    iptables -t nat -I PREROUTING 2 -p all -s 10.42.0.105 -j ACCEPT
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+else echo -n "[ERROR] $OSTYPE is not support!"
+fi
+
