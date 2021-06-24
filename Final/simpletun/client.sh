425ddc0c712b06af7f6a941a2fae5dde3a465ab2
tingwei-fly <b06902130@ntu.edu.tw>
2021-06-24 17:39:31 +0800

    updata client server

--- Final/simpletun/client.sh
+++ Final/simpletun/client.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-ip tuntap add tun$1 mode tun
+sudo ip tuntap add tun$1 mode tun
 echo "[INFO] Add new tun"
-ip link set tun$1 up
-ip addr add $2/24 dev tun$1
-./tun -i tun$1 -c $3
+sudo ip link set tun$1 up
+sudo ip addr add $2/24 dev tun$1
+sudo ./tun -i tun$1 -c $3
+echo "connect to server"
